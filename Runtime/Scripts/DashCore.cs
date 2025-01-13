637ff96f7d7dac960e2e58dc879a9371ffa370fd
Peter @sHTiF Stefcek <pshtif@gmail.com>
2025-01-13 17:44:02 +0100

    update version 0.14.11

--- Runtime/Scripts/DashCore.cs
+++ Runtime/Scripts/DashCore.cs
@@ -15,7 +15,7 @@ namespace Dash
 {
     public class DashCore
     {
-        public static string VERSION = "0.14.10";
+        public static string VERSION = "0.14.11";
         
         public DashRuntimeConfig Config { get; private set; }
         
